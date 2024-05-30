b3f8f90ee66065e12b565a7ab85d293b92951ea2
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-05-30 16:57:32 +0600

    keyboard key showing on screen and keyboard highlight also

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -2,7 +2,13 @@ function continueGame()
 {
     // Generate a RandomAlpabet Function
     const alphabet = getRandomAlphabet();
-    console.log("You alphabet is : ", alphabet);
+    // console.log("You alphabet is : ", alphabet);
+
+    // shown the alphabet
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerHTML = alphabet;
+
+    setBackgroundColorById(alphabet);
 }
 function play()
 {
--- script/utility.js
+++ script/utility.js
@@ -8,6 +8,11 @@ function showElementById(elementId)
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+function setBackgroundColorById(elementId)
+{
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400')
+}
 function getRandomAlphabet()
 {
     const alphabetString ='abcdefghijklmnopqrstuvwxyz';
