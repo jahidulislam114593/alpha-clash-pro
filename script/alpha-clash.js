3bc2fc541e91d7e28850d9f28306e20c3d24b819
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-05-30 16:28:05 +0600

    created randomAlphabet function

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,5 +1,12 @@
+function continueGame()
+{
+    // Generate a RandomAlpabet Function
+    const alphabet = getRandomAlphabet();
+    console.log("You alphabet is : ", alphabet);
+}
 function play()
 {
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame();
 }
--- script/utility.js
+++ script/utility.js
@@ -8,3 +8,17 @@ function showElementById(elementId)
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+function getRandomAlphabet()
+{
+    const alphabetString ='abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets);
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+    // console.log(index);
+
+    const alphabet = alphabets[index];
+    // console.log(index, alphabet);
+    return alphabet;
+}
